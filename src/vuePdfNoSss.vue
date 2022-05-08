e56677dd34dba1cca0cfe1a7832679f10d48ab0d
Godwin Agedah <agedah99@gmail.com>
2022-05-08 21:59:34 +0100

    Update vuePdfNoSss.vue

--- src/vuePdfNoSss.vue
+++ src/vuePdfNoSss.vue
@@ -5,12 +5,12 @@
 
 	if ( import.meta.env.VUE_ENV !== 'server' ) {
 
-		var pdfjsWrapper = require('./pdfjsWrapper.js').default;
-		var PDFJS = require('pdfjs-dist/es5/build/pdf.js');
+		var pdfjsWrapper = import('./pdfjsWrapper.js').default;
+		var PDFJS = import('pdfjs-dist/es5/build/pdf.js');
 
 		if ( typeof window !== 'undefined' && 'Worker' in window && navigator.appVersion.indexOf('MSIE 10') === -1 ) {
 
-			var PdfjsWorker = require('worker-loader!pdfjs-dist/es5/build/pdf.worker.js');
+			var PdfjsWorker = import('worker-loader!pdfjs-dist/es5/build/pdf.worker.js');
 			PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
 		}
 
